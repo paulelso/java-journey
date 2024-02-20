89f707c6c30909da8c1dfcd6d5639647606095bb
psousa@twilio.com <paulelso@hotmail.com>
2024-02-20 22:45:34 +0100

    Changes to sketch.js.

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
+let angle = 0;
+
 function setup() {
   createCanvas(400, 400);
-  background(200);
-  noLoop();
+  rectMode(CENTER);
+  angleMode(DEGREES);
 }
 
 function draw() {
-    background(200);
-    rect(width/2, height/2, 100, 50);
+  background(220);
+  let distance = dist(mouseX, mouseY);
+  
+  translate(width/2, height/2);
+  rotate(angle);
+  rect(0, 0, 100, 50);
+  
+  angle += 1;
 }
